8bf841b28175025cbc4340b316f8e79422ffd9db
gst <pavhanys@gmail.com>
2024-10-29 21:22:26 +0100

    feat: add Button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Button = ({ children }) => {
+  return (
+      <button className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-lg text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 bg-blue-800 text-white hover:bg-blue-800/80 h-10 px-4 py-2">
+        {children}
+      </button>
+  );
+}
